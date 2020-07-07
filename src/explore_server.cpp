a474802fb756127d546b0bd3eeec7873042e242e
mrunaljsarvaiya <mrunaljsarvaiya@gmail.com>
2020-07-06 18:14:34 -0700

    Add reset costmap layers service

--- src/explore_server.cpp
+++ src/explore_server.cpp
@@ -1,8 +1,9 @@
 #include <ros/ros.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <costmap_2d/costmap_2d.h>
-
 #include <tf/transform_listener.h>
+
+#include<std_srvs/Trigger.h> 
 
 namespace frontier_exploration{
 
@@ -22,6 +23,10 @@ public:
         tf_listener_(ros::Duration(10.0)),
         private_nh_("~"){
         explore_costmap_ros_ = boost::shared_ptr<costmap_2d::Costmap2DROS>(new costmap_2d::Costmap2DROS("explore_costmap", tf_listener_));
+        explore_costmap_ros_->resetLayers();
+
+        // Start services 
+        reset_server_ = private_nh_.advertiseService("reset_costmap", &FrontierExplorationServer::resetCostmapCb, this);
     }
 
 private:
@@ -29,17 +34,26 @@ private:
     tf::TransformListener tf_listener_;
     boost::shared_ptr<costmap_2d::Costmap2DROS> explore_costmap_ros_;
    
+    // Service servers
+    ros::ServiceServer reset_server_;
+    
+
+    /**
+     * @brief Reset costmap layers 
+     */
+    bool resetCostmapCb(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res){
+        explore_costmap_ros_->resetLayers();
+        res.success = true;
+        ROS_INFO("Reset costmap layers");
+        return true;
+    }
+
 };
 
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "explore_server");
-    // if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info) ) {
-    //   ros::console::notifyLoggerLevelsChanged();
-    // }
-    // ros::console::set_logger_level("ros.costmap_2d", ros::console::levels::Error);
-
     frontier_exploration::FrontierExplorationServer server(ros::this_node::getName());
     
     ros::spin();
